cdcde2cc9ce449cab3f2fd24e5442ecd195485fc
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-03-31 16:03:12 +0300

    Added templates for Product model

--- store_service/urls.py
+++ store_service/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from core.views import frontpage
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", frontpage, name="frontpage"),
+    path("products/", include("product.urls", namespace="products")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
