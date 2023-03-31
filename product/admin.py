0ffafbf6a07ebaee5a814b441ae5ab1ffb2b7187
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-03-31 15:49:14 +0300

    Added models of Product, Category and Review

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from product.models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug",)
+    prepopulated_fields = {"slug": ("name",)}
+    search_fields = ("name",)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "slug",
+        "image_show",
+        "price",
+        "is_available",
+        "inventory",
+        "created_at",
+        "updated_at"
+    )
+    list_filter = ("is_available", "created_at", "updated_at")
+    list_editable = ("price", "is_available")
+    prepopulated_fields = {"slug": ("name",)}
+    search_fields = ("name",)
+
+    def image_show(self, obj):
+        if obj.image:
+            return mark_safe(
+                "<img src='{}' width='60' />".format(obj.image.url)
+            )
+        return "None"
+
+    image_show.__name__ = "Picture"
--- product/models.py
+++ product/models.py
@@ -1,3 +1,75 @@
+from django.conf import settings
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, db_index=True)
+    slug = models.SlugField(max_length=255, db_index=True, unique=True)
+
+    class Meta:
+        ordering = ("name",)
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse(
+            "product:product-list-by-category",
+            kwargs={"category_slug": self.slug}
+        )
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name="products"
+    )
+    name = models.CharField(max_length=255, db_index=True)
+    slug = models.SlugField(max_length=255, db_index=True)
+    image = models.ImageField(upload_to="products/%Y/%m/%d", blank=True)
+    description = models.TextField(blank=True)
+    inventory = models.PositiveIntegerField(default=5)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ("name",)
+        index_together = (("id", "slug"),)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("product:product-detail", args=[self.id, self.slug])
+
+    def get_rating(self):
+        reviews_total = 0
+
+        for review in self.reviews.all():
+            reviews_total += review.rating
+
+        if reviews_total > 0:
+            return reviews_total / self.reviews.count()
+
+        return 0
+
+
+class Review(models.Model):
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="reviews"
+    )
+    rating = models.IntegerField(default=3)
+    content = models.TextField(blank=True)
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="reviews"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ("-created_at",)
